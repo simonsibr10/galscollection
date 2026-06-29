9be934f7a67fbcd3d472d0fe80b376d1a92249ea
Laptop <laptop@samadibali.com>
2026-06-29 23:05:54 +0700

    menambahkan user name dan password

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_name = 'mysql:host=localhost;dbname=shop_db';
-$user_name = 'root';
-$user_password = '';
+$user_name = 'galscollection';
+$user_password = 'Vnx?8^BBkwoif7i1';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
