87c3b11f72bdd13683afe5cc5ed554c5ceef05e1
Laptop <laptop@samadibali.com>
2026-07-01 15:03:55 +0700

    masukkan css admin ke admin_style.css

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -34,6 +34,7 @@ if(isset($_POST['submit'])){
    <title>Admin Login — Gals Collection</title>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <link rel="stylesheet" href="../css/style.css">
+   <link rel="stylesheet" href="../css/admin_style.css">
 </head>
 <body class="php-admin-admin-login">
 
